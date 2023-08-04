4a4703e1dd3639e9d66f1f7c780a57d25190c21d
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-08-04 20:41:51 +0200

    Function Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
-	const handleNameChange = () => {
-		const names = ['Radek', 'Emilia', 'Dawid', 'Miłosz'];
-		return names[Math.floor(Math.random() * names.length)];
-	};
-
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Hello {handleNameChange()}!</p>
-			</header>
+			<Header />
+			<Content />
+			<Footer />
 		</div>
 	);
 }
---
+++ src/Content.jsx
@@ -0,0 +1,11 @@
+export default function Content() {
+	const handleNameChange = () => {
+		const names = ['Radek', 'Emilia', 'Dawid', 'Miłosz'];
+		return names[Math.floor(Math.random() * names.length)];
+	};
+	return (
+		<main>
+			<p>Hello {handleNameChange()}</p>
+		</main>
+	);
+}
