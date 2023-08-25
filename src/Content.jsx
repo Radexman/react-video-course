74640cc6e738efe40e837eac8e2997c055d6cf87
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-08-25 13:25:15 +0200

    List & Keys

--- src/Content.jsx
+++ src/Content.jsx
@@ -8,30 +8,51 @@ export default function Content() {
 
 	const handleCheck = (id) => {
 		const listItems = items.map((item) => (item.id === id ? { ...item, checked: !item.checked } : item));
+
 		setItems(listItems);
+
+		localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+	};
+
+	const handleDlete = (id) => {
+		const listItems = items.filter((item) => item.id !== id);
+
+		setItems(listItems);
+
+		localStorage.setItem('shoppinglist', JSON.stringify(listItems));
 	};
 
 	return (
 		<main>
-			<ul>
-				{items.map((item) => (
-					<li
-						className='item'
-						key={item.id}
-					>
-						<input
-							type='checkbox'
-							checked={item.checked}
-							onChange={() => handleCheck(item.id)}
-						/>
-						<label>{item.value}</label>
-						<FaTrashAlt
-							role='button'
-							tabIndex='0'
-						/>
-					</li>
-				))}
-			</ul>
+			{items.length ? (
+				<ul>
+					{items.map((item) => (
+						<li
+							className='item'
+							key={item.id}
+						>
+							<input
+								type='checkbox'
+								checked={item.checked}
+								onChange={() => handleCheck(item.id)}
+							/>
+							<label
+								onDoubleClick={() => handleCheck(item.id)}
+								style={item.checked ? { textDecoration: 'line-through' } : null}
+							>
+								{item.value}
+							</label>
+							<FaTrashAlt
+								role='button'
+								tabIndex='0'
+								onClick={() => handleDlete(item.id)}
+							/>
+						</li>
+					))}
+				</ul>
+			) : (
+				<p style={{ marginTop: '2rem' }}>Your list is empty</p>
+			)}
 		</main>
 	);
 }
