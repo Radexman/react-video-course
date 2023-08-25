13a7d105c8d83d03c1e417b048f0b967ef63dc2f
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-08-25 12:51:30 +0200

    Checked State

--- src/Content.jsx
+++ src/Content.jsx
@@ -6,14 +6,29 @@ import data from './data';
 export default function Content() {
 	const [items, setItems] = useState(data);
 
+	const handleCheck = (id) => {
+		const listItems = items.map((item) => (item.id === id ? { ...item, checked: !item.checked } : item));
+		setItems(listItems);
+	};
+
 	return (
 		<main>
 			<ul>
 				{items.map((item) => (
-					<li className="item" key={item.id}>
-						<input type="checkbox" checked={item.checked} />
+					<li
+						className='item'
+						key={item.id}
+					>
+						<input
+							type='checkbox'
+							checked={item.checked}
+							onChange={() => handleCheck(item.id)}
+						/>
 						<label>{item.value}</label>
-						<FaTrashAlt role="button" tabIndex="0" />
+						<FaTrashAlt
+							role='button'
+							tabIndex='0'
+						/>
 					</li>
 				))}
 			</ul>
